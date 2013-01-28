4d9a2256d33d35681de33c20fb43e80abd325727
mulog123 <dvnk123@hotmail.com>
2013-01-27 22:15:22 -0800

    q5_upper_to_lower_final.py

---
+++ q5_upper_to_lower.py
@@ -0,0 +1,8 @@
+#Program: Converting an uppercase letter to lowercase
+#By: David Fan
+
+#Requesting user to input uppercase letter
+uppercaseletter = input("Hello! Input uppercase letter to convert to lowercase letter")
+#Equation to convert uppercase letter to lowercase letter
+lowercaseletter = uppercaseletter.lower()
+print(lowercaseletter)
